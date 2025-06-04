3315739e4f5cce46e99bee5bc4a8c153aa8eb58a
ziac007 <dinesh.bdragon@gmail.com>
2025-06-04 10:19:43 +0530

    Testing to remove warning while deployment in render

--- keepalive.py
+++ keepalive.py
@@ -1,15 +1,7 @@
 from flask import Flask
-from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "✅ Bot is running!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
